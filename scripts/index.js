a35a2e07dacb253e865c86e833326cb16e1b69e1
Sofya Mitrofanova <sonyamitrofanova3262@gmail.com>
2022-07-12 17:15:51 +0500

    Refactoring: убрала горизонтальный скролл, отредактировала попап и блок с каринками

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+let openPopUp = document.querySelector('.profile__button-edit');
+let closePopUp = document.querySelector('.popup__close');
+let popUp = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_about');
+let saveButton = document.querySelector('.popup_button');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
+
+function openHandler() {
+  popUp.classList.add('popup_opened');
+}
+
+function closeHandler() {
+  popUp.classList.remove('popup_opened');
+}
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileAbout.textContent = jobInput.value;
+  popUp.classList.remove('popup_opened');
+}
+
+openPopUp.addEventListener('click', openHandler);
+closePopUp.addEventListener('click', closeHandler);
+formElement.addEventListener('submit', formSubmitHandler);
