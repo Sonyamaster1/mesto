1b28f06de2e56bc7d1c1b2a1232acdd8ccb46330
Sofya Mitrofanova <sonyamitrofanova3262@gmail.com>
2022-07-13 12:05:32 +0500

    Refactoring: убрала горизонтальный скролл иотцентрировала блок

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,6 @@ let popUp = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__form');
 let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_about');
-let saveButton = document.querySelector('.popup_button');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 
@@ -19,7 +18,8 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileAbout.textContent = jobInput.value;
-  popUp.classList.remove('popup_opened');
+  closeHandler();
+  /*popUp.classList.remove('popup_opened');*/
 }
 
 openPopUp.addEventListener('click', openHandler);
